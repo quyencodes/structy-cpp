1687a8e4451bb6ca3d5af1b9aaeb0691f9854fb3
Quyen Hoang <qduhoang@gmail.com>
2023-10-30 17:25:03 -0700

    heyProgrammer complete

---
+++ 0-intro/heyProgrammer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+std::string greet(std::string s) {
+  // todo
+  return "hey " + s;
+}
+
+
+void run() {
+  // this function behaves as `main()` for the 'run' command
+  // you may sandbox in this function, but should not remove it
+  std::cout << "hi my name is" << std::endl;
+  std::cout << "quyen hoang" << std::endl;
+}
