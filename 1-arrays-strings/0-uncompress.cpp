713c09926a018a0d142068f61b940d83ccc43fe1
Quyen Hoang <qduhoang@gmail.com>
2023-11-02 13:36:59 -0700

    uncompress complete

--- 1-arrays-strings/0-uncompress.cpp
+++ 1-arrays-strings/0-uncompress.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
+#include <limits>
 #include <string>
 
 std::string uncompress(std::string s) {
-  std::string res = "";
-  std::string nums = "0123456789";
-  int i = 0;
-  int j = 0;
+  std::string result = "", numbers = "0123456789";
+  int i = 0, j = 0;
+
   while (j < s.length()) {
-    if (nums.find(s[j]) != std::string::npos) {
+    if (numbers.find(s[j]) != std::string::npos) {
       j += 1;
     } else {
-      int num = std::stoi(s.substr(i, j - i));
-      res += std::string(num, s[j]);
+      int count = std::stoi(s.substr(i, j-i));
+      // for (int n = 0; n < count; n++) {
+      //   result += s[j];
+      // }
+      result += std::string(count, s[j]);
       i = j + 1;
       j = i;
     }
   }
-
-  return res;
+  return result;
 }
+
 
 void run() {
   // this function behaves as `main()` for the 'run' command
   // you may sandbox in this function, but should not remove it
-
 }
